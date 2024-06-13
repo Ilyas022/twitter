10f8fadd08ff68b1ecad5b696777340a7ebfd4af
Ilyas022 <ilya.shchetko@mail.ru>
2024-06-13 16:34:47 +0300

    feat: add adaptive layout to sign up page

--- src/pages/SignUpPage/styled.ts
+++ src/pages/SignUpPage/styled.ts
@@ -1,16 +1,26 @@
 import styled from 'styled-components'
 
 import Logo from 'assets/icons/logoIcon.svg?react'
+import { device } from 'constants/breakpoints'
 import { getColors, getFontWeights, getFonts, getIndents, getSizes } from 'utils/themeGetters'
 
 export const Page = styled.main``
 
 export const Container = styled.div`
 	width: ${(props) => getSizes(props, 13, 10)};
-	height: ${(props) => getSizes(props, 13, 130)};
 	margin: 0 auto;
-	padding: ${(props) => getIndents(props, 4, 6)} ${(props) => getIndents(props, 5)}
-		${(props) => getIndents(props, 5)};
+	// prettier-ignore
+	padding: ${(props) => getIndents(props, 4, 6)} ${(props) => getIndents(props, 5)}	${(props) =>
+		getIndents(props, 5)};
+
+	@media ${device.lg} {
+		width: 70vw;
+	}
+
+	@media ${device.md} {
+		width: 95vw;
+		padding: ${(props) => getIndents(props, 2)};
+	}
 `
 
 export const LogoIcon = styled(Logo)`
@@ -18,10 +28,18 @@ export const LogoIcon = styled(Logo)`
 	height: ${(props) => getSizes(props, 1, 1)};
 	margin-bottom: ${(props) => getIndents(props, 5, -4)};
 	color: ${(props) => getColors(props).primary};
+
+	@media ${device.md} {
+		margin-bottom: ${(props) => getIndents(props, 3)};
+	}
 `
 
 export const Title = styled.h1`
 	font-weight: ${(props) => getFontWeights(props, 3, 100)};
 	font-size: ${(props) => getFonts(props, 5)};
 	margin-bottom: ${(props) => getIndents(props, 5)};
+
+	@media ${device.md} {
+		margin-bottom: ${(props) => getIndents(props, 2)};
+	}
 `
