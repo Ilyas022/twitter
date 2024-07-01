d6d70d2b43607128e659ceb03a3c0295ff24104e
Ilyas022 <ilya.shchetko@mail.ru>
2024-07-01 03:04:33 +0300

    fix: fix default values of edit profile pop up

--- src/components/PopUps/EditProfilePopUp/index.tsx
+++ src/components/PopUps/EditProfilePopUp/index.tsx
@@ -38,13 +38,17 @@ function EditProfilePopUp({ onClose }: EditProfilePopUpProps) {
 	useEffect(() => {
 		if (value) {
 			const { about, birthDate, email, name, phone, surname, tag } = value as UserData
+			let formattedDate = ''
 
-			const date = new Date((birthDate as Timestamp).toDate())
-			const year = date.getFullYear()
-			const month = String(date.getMonth() + 1).padStart(2, '0')
-			const day = String(date.getDate()).padStart(2, '0')
+			if (birthDate) {
+				const date = new Date((birthDate as Timestamp).toDate())
+				const year = date.getFullYear()
+				const month = String(date.getMonth() + 1).padStart(2, '0')
+				const day = String(date.getDate()).padStart(2, '0')
 
-			const formattedDate = `${year}-${month}-${day}`
+				formattedDate = `${year}-${month}-${day}`
+			}
+
 			reset({
 				about,
 				birthDate: formattedDate as unknown as Date,
